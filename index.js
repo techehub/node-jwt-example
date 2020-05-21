082bd8b9453a5a855a7eab8c7dedaff77ec550b0
itr <1>
2020-05-21 09:11:39 +0530

    sample project changes

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 const { signIn, welcome, refresh } = require("./handlers")
 app.use(bodyParser.json())
 
+var cors = require('cors');
+app.use(cors());
+
 app.post("/signin", signIn)
 app.get("/welcome", welcome)
 app.post("/refresh", refresh)
